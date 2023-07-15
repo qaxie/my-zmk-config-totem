dea60ec343a0faeb19fd55788794e998c0d539a2
austin-jrh <austin.jrh@gmail.com>
2023-07-15 11:55:23 +0800

    Edited readme, attempting to correct keypos.

--- zmk-nodefree-config/keypos_def/keypos_38keys.h
+++ zmk-nodefree-config/keypos_def/keypos_38keys.h
@@ -3,7 +3,7 @@
      ╭─────────────────────┬────────────────────╮         ╭──────────────────────┬──────────────────────╮
      │   0   1   2   3   4 │  5   6   7   8   9 │         │  LT4 LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3 RT4  │
   ╭──╯  10  11  12  13  14 │ 15  16  17  18  19 ╰───╮  ╭──╯  LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4  ╰──╮
-  │ 30  20  21  22  23  24 │ 25  26  27  28  29  39 │  │ LB5 LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 RB5 │
+  │ 20  21  22  23  24  25 │ 26  27  28  29  30  31 │  │ LB5 LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 RB5 │
   ╰──────────╮  32  33  34 │ 35  36  37 ╭───────────╯  ╰──────────╮  LH2 LH1 LH0 │ RH0 RH1 RH2 ╭───────────╯
              ╰─────────────┴────────────╯                         ╰──────────────┴─────────────╯             */
 
@@ -33,19 +33,19 @@
 #define RM3 18
 #define RM4 19
 
-#define LB0 24  // left-bottom row
-#define LB1 23
-#define LB2 22
-#define LB3 21
-#define LB4 20
-#define LB5 30
+#define LB0 25  // left-bottom row
+#define LB1 24
+#define LB2 23
+#define LB3 22
+#define LB4 21
+#define LB5 20
 
-#define RB0 25  // right-bottom row
-#define RB1 26
-#define RB2 27
-#define RB3 28
-#define RB4 29
-#define RB5 39
+#define RB0 26  // right-bottom row
+#define RB1 27
+#define RB2 28
+#define RB3 29
+#define RB4 30
+#define RB5 31
 
 #define LH0 34  // left thumb keys
 #define LH1 33
